67713603f0746c609e52ed21b378c9e8753dc02b
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-03-30 18:51:09 +0600

    nav bar done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import "./index.css";
+import Nav from "./Nav.jsx";
+
+function App() {
+  return (
+      <Nav />
+  )
+  
+}
+
+export default App;
